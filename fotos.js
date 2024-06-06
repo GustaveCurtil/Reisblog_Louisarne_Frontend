dad1d7931b2b0e902a2a3be2618f0475214b5620
Gustave Curtil <gustave.curtil@tutanota.com>
2024-06-06 20:47:12 +0200

    foto's klikbaar maken: eerste stap

--- fotos.js
+++ fotos.js
@@ -1,3 +1,5 @@
+let pagewrapper = document.querySelector('#pagewrapper');
+let fotogalerij = document.querySelector("#fotogalerij");
 let kolommen = document.querySelectorAll(".kolom");
 let fotos = document.querySelectorAll(".fotos > img")
 let kolom1 = kolommen[0];
@@ -14,8 +16,8 @@ console.log(fotos)
 
 console.log(percentageToPixels(window, 2.5));
 
-
-fotos.forEach(foto => {
+for (let i = 0; i < fotos.length; i++) {
+    const foto = fotos[i];
     document.documentElement.style.setProperty('--randmarge', percentageToPixels(window, 2.5) + 'px');
     if (kolom1hoogte <= kolom2hoogte && kolom1hoogte <= kolom3hoogte) {
         kolom1.appendChild(foto);  
@@ -27,7 +29,13 @@ fotos.forEach(foto => {
         kolom3.appendChild(foto);
         kolom3hoogte += foto.clientHeight + percentageToPixels(window, 2.5) ;  
     }
-});
+
+    foto.addEventListener('click', (e) => {
+        pagewrapper.classList.remove('visible');
+        fotogalerij.classList.add('visible');
+    })
+}
+
 
 function percentageToPixels(container, percent) {
     return (container.innerWidth * percent) / 100;
